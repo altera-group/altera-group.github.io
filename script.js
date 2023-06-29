14be1f7b4727d246b67469aaa388b6a8ca7e8d62
Suchys22 <87770666+Suchys22@users.noreply.github.com>
2023-06-29 21:59:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,56 @@
-const axios = require('axios');
-const baseUrl = "https://api.mcsrvstat.us/";
+const toggleButton = document.getElementsByClassName('toggle')[0]
+const navbarLinks = document.getElementsByClassName('nav')[0]
+const ipButton = document.getElementsByClassName('ipcont')[0]
 
-async function fetchJavaServer(ip) {
-    if (!ip) throw new Error("Minecraft Java IP not supplied");
-    else {
-        return axios(baseUrl + `2/${ip}`).then((response) => {
-            return {
-                online: response.data.online,
-                version: response.data.version,
-                players: response.data.players,
-                connection: {
-                    ip: response.data.ip,
-                    hostname: response.data.hostname || null
-                }
-            }
-        }).catch((e) => {
-            throw new Error(`Failed to fetch '${ip}'`, {cause:e})
-        })
-    }
-}
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('hide')
+  toggleButton.classList.toggle('toggle_anim')
+})
+ipButton.addEventListener('click', () => {
+  ipcopy()
+})
 
-async function fetchBedrockServer(ip) {
-    if (!ip) throw new Error("Minecraft Bedrock IP not supplied");
-    return axios(baseUrl + `bedrock/2/${ip}`).then((response) => {
-        return {
-            online: response.data.online,
-            version: response.data.version,
-            players: response.data.players,
-            connection: {
-                ip: response.data.ip,
-                hostname: response.data.hostname || null
-            }
-        }
-    }).catch((e) => {
-        throw new Error(`Failed to fetch '${ip}'`, {cause:e})
-    })
-}
 
-async function fetchIcon(ip) {
-    return `https://api.mcsrvstat.us/icon/${ip}`;
-}
 
-async function javaHttpCode(ip) {
-    return axios(baseUrl + `simple/${ip}`).then((response) => {
-        return response.status;
-    }).catch((e) => {
-        if (e.toJSON && e.toJSON().status === 404) return 404;
-        else throw new Error(`An unexpected status code/error was received while fetching '${ip}'`, {cause:e})
-    })
-}
 
-const bedrockHttpCode = async function(ip) {
-    return axios(baseUrl + `bedrock/simple/${ip}`).then((response) => {
-        return response.status;
-    }).catch((e) => {
-        if (e.toJSON && e.toJSON().status === 404) return 404;
-        else throw new Error(`An unexpected status code/error was received while fetching '${ip}'`, {cause:e})
-    })
-}
 
-module.exports = {fetchJavaServer, fetchBedrockServer, fetchIcon, javaHttpCode, bedrockHttpCode}
+function ipcopy() {
+  // Copy the text inside the text field
+ navigator.clipboard.writeText("play.alteracraft.eu");
+ alert("IP adresa serveru byla zkopírována");
+} 
+
+
+
+
+
+//fetch mc
+var url = "https://api.minetools.eu/ping/185.137.94.42/25617";
+ 
+ $.getJSON(url, function(r) {
+     //data is the JSON string
+  if(r.error){
+     $('#iptext').html('<b1>Server Offline</b1>');
+    return false;
+  } 
+ var pl = '';
+   $('#iptext').html('Online: '+r.players.online);
+
+     
+ });
+
+
+//fetchdis
+
+ var url2 = "https://discord.com/api/guilds/426690734129938434/widget.json";
+ $.getJSON(url2, function(d) {
+     //data is the JSON string
+  if(d.error){
+     $('#rest2').html('Server Offline');
+    return false;
+  } 
+ var pld = '';
+   $('#rest2').html('<h2>DISCORD:</h2> <b>Uživatelé online: '+d.members.length) +'</b>';
+
+     
+ });
